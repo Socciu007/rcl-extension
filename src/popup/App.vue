48c4f00039cd10eae2141f8b7efec841d818f33c
Money <manhtien310701@gmail.com>
2024-09-09 14:50:34 +0800

    add mock-data

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -63,22 +63,219 @@ export default {
     async handleSubmit() {
       const data = { 'ebNo': this.inputValue };
       try {
-        const response = await axios({
-          // url: "http://localhost:3001/cargosmart/edi/seaOrder",
-          url: "http://localhost:3000/moneyapi/getOnefreight",
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          data: JSON.stringify(data),
-        });
+        // Define data test
+        let globalData = {
+          id: 150,
+          userId: null,
+          ebNo: 'FYJK000020-150',
+          type: false,
+          self: false,
+          spell: false,
+          startPort: 599,
+          uploadPort: 3108,
+          purposePort: 3108,
+          shipCompany: 9,
+          sailingTime: '2023-11-17',
+          putType: 0,
+          declare: '',
+          interior: false,
+          trailer: false,
+          trailerTime: null,
+          custom: false,
+          entrustNub: '',
+          ladingCount: 0,
+          ladingNub: '',
+          blNo: '1345',
+          blNoTsl: '11111',
+          putCode: '',
+          freightTerms: false,
+          transportTerms: '',
+          appointNub: '',
+          matchTaime: '',
+          shipName: 'KTY 2413S',
+          voyage: 'RNV2',
+          portVoyage: '',
+          bindingNotice: '',
+          startPortTime: '',
+          stopPortTime: '',
+          inPortCode: '',
+          overTime: '',
+          stopPassTime: '',
+          main: true,
+          tdRemark: null,
+          shipAgency: 'COSCO',
+          leak: 0,
+          canPut: 0,
+          startPortalias: null,
+          purposePortalias: null,
+          uploadPortalias: null,
+          trailerAddress: '',
+          tetd: '',
+          vgmendTime: '',
+          addspell: null,
+          qy: 0,
+          dcstatus: 0,
+          tdstatus: 0,
+          cdstatus: 0,
+          vdstatus: 0,
+          cdreceipt: null,
+          ebId: 0,
+          receiptRemark: null,
+          endPortPierId: null,
+          xhxxStatus: 0,
+          mentionCon: null,
+          managelcl: false,
+          outside: 2,
+          icn: null,
+          warehouseName: null,
+          realityLeavePortTime: null,
+          realityClosePortTime: null,
+          exportBoxStartTime: null,
+          isf5: '0',
+          emptyId: null,
+          acidNumber: null,
+          vatNumber: null,
+          egyptAcidNumber: null,
+          createTime: '2023-11-16T09:00:07.000Z',
+          updateTime: '2024-03-04T05:43:09.000Z',
+          routeId: null,
+          containner_vgms: [
+            {
+              cType: 40,
+              boxNub: 'TEST1111233',
+              weight: '0.000',
+              volume: '0.000',
+              sealNUb: '',
+              amount: 10,
+              hostMark: true,
+              method: false
+            }
+          ],
+          sea_order_bind: { routeId: 0, routeCode: 'SKT' },
+          sea_order_contacts: [
+            {
+              id: 181523,
+              oid: 150,
+              cabinId: 0,
+              lclid: 0,
+              type: 2,
+              master: 'SAME AS CONSIGNEE',
+              address: '',
+              firmCode: null,
+              country: null,
+              phone: null,
+              aeo: null,
+              spName: null,
+              spType: null,
+              spContact: null,
+              qy: 0,
+              createTime: '2023-11-16T09:00:07.000Z',
+              updateTime: '2024-03-04T05:43:09.000Z'
+            },
+            {
+              id: 181522,
+              oid: 150,
+              cabinId: 0,
+              lclid: 0,
+              type: true,
+              master: 'ASAHI AUTO SPARE PARTS L.L.C',
+              address: 'SHOP NO 18, NAIF 7 STATES BLDG, \n' +
+                'BEHIND KHANSAHEB PARKING, DIERA \n' +
+                'DUBAI,UNITED ARAB EMIRATES',
+              firmCode: null,
+              country: null,
+              phone: null,
+              aeo: null,
+              spName: null,
+              spType: null,
+              spContact: null,
+              qy: 0,
+              createTime: '2023-11-16T09:00:07.000Z',
+              updateTime: '2024-03-04T05:43:09.000Z'
+            },
+            {
+              id: 181521,
+              oid: 150,
+              cabinId: 0,
+              lclid: 0,
+              type: false,
+              master: 'YANCHENG RELY IMPORT & EXPORT \nCORP., LTD.',
+              address: 'RM.401 TOWER #16,ZHONG YIN HAIHUA \n' +
+                'PLAZA NO.59,JIANJUN\n' +
+                'RD.,YANCHENG,CHINA',
+              firmCode: null,
+              country: null,
+              phone: null,
+              aeo: null,
+              spName: null,
+              spType: null,
+              spContact: null,
+              qy: 0,
+              createTime: '2023-11-16T09:00:07.000Z',
+              updateTime: '2024-03-04T05:43:09.000Z'
+            }
+          ],
+          goods: [
+            {
+              id: 306,
+              oid: 150,
+              cabinId: 0,
+              lclid: 0,
+              shantou: null,
+              name: 'TEST!!!',
+              description: '你好',
+              cargoType: false,
+              amount: '10',
+              unit: 'BAGS',
+              weight: '2000.000',
+              volume: '1111.000',
+              hscode: '160413',
+              dangerClass: null,
+              unNub: null,
+              dangerCode: null,
+              dangerPoint: '0',
+              packType: null,
+              packDesc: null,
+              packWeight: null,
+              packSize: null,
+              grossWeight: null,
+              quarantineCode: null,
+              quarantineName: null,
+              sign: null,
+              qy: 0,
+              conid: null,
+              createTime: '2023-11-16T09:00:07.000Z',
+              updateTime: '2024-03-04T05:43:09.000Z'
+            }
+          ],
+          startPortData: { id: 599, name: '上海', nameEn: 'SHANGHAI', code: 'CNSHA' },
+          purposePortData: { id: 3108, name: '新加坡', nameEn: 'HO CHI MINH CITY', code: 'VNSGN' },
+          uploadPortsea: { id: 3108, name: '新加坡', nameEn: 'HO CHI MINH CITY', code: 'VNSGN' },
+          carrier: {
+            id: 9,
+            code: 'COSCO',
+            name: '中远',
+            nameEn: 'COSCO',
+            scacCode: 'COSU'
+          }
+        }
+        // Define fetch data from interface
+        // const response = await axios({
+        //   // url: "http://localhost:3001/cargosmart/edi/seaOrder",
+        //   url: "http://localhost:3000/moneyapi/getOnefreight",
+        //   method: "GET",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //   },
+        //   data: JSON.stringify(data),
+        // });
         // 发送到content.js
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
           chrome.tabs.sendMessage(tabs[0].id, { action: "login" });
         });
 
         // Sent msg to background.js to init automation page
-        chrome.runtime.sendMessage({ action: 'startAuto', data: response.data.data });
+        chrome.runtime.sendMessage({ action: 'startAuto', data: globalData });
         return response.data;  // 返回数据用于进一步处理
       } catch (error) {
         console.error("Error during submission:", error);  // 打印错误信息
